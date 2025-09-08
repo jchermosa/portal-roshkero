d3df97f5d9acfb463b5efc174d5876fae6fb6356
ZenKySSJ <78515792+zenkyssj@users.noreply.github.com>
2025-09-08 15:10:28 -0300

    JPA implementado en los Repositorys

--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/admin/repository/HumanResourceRepository.java
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/admin/repository/HumanResourceRepository.java
@@ -1,4 +1,10 @@
 package com.backend.portalroshkabackend.admin.repository;
 
-public class HumanResourceRepository {
+
+import com.backend.portalroshkabackend.common.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HumanResourceRepository extends JpaRepository<Usuario, Integer> {
 }
--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/admin/repository/OperationRepository.java
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/admin/repository/OperationRepository.java
@@ -1,4 +1,10 @@
 package com.backend.portalroshkabackend.admin.repository;
 
-public class OperationRepository {
+
+import com.backend.portalroshkabackend.common.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OperationRepository extends JpaRepository<Usuario, Integer> {
 }
