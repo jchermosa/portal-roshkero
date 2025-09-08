b5a335be1166e1ffd6fe4465cad30b95954d2f7a
ZenKySSJ <78515792+zenkyssj@users.noreply.github.com>
2025-09-08 14:50:45 -0300

    Clases Repositorio creadas para cada tipo de administrador en admin/repository

---
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/admin/repository/SystemAdminRepository.java
@@ -0,0 +1,4 @@
+package com.backend.portalroshkabackend.admin.repository;
+
+public class SystemAdminRepository {
+}
--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/admin/service/IHumanResourceService.java
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/admin/service/IHumanResourceService.java
@@ -1,4 +1,8 @@
 package com.backend.portalroshkabackend.admin.service;
 
 public interface IHumanResourceService {
+
+
+
+
 }
