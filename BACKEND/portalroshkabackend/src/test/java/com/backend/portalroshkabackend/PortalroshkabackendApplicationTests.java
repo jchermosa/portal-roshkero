e212343d159ff17a06246dd3ee28747e4988d179
xdev <xdevofc@gmail.com>
2025-09-08 10:58:40 -0300

    inicializando el backend

---
+++ BACKEND/portalroshkabackend/src/test/java/com/backend/portalroshkabackend/PortalroshkabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.portalroshkabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalroshkabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
