27ada173f6f47fec74cb579be687420faaf4c904
ZenKySSJ <78515792+zenkyssj@users.noreply.github.com>
2025-09-08 14:38:20 -0300

    Creando Modelo Cargos en common/model

---
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/common/model/Cargos.java
@@ -0,0 +1,23 @@
+package com.backend.portalroshkabackend.common.model;
+
+public class Cargos {
+    private int id_cargo;
+
+    private String nombre;
+
+    public int getId_cargo() {
+        return id_cargo;
+    }
+
+    public void setId_cargo(int id_cargo) {
+        this.id_cargo = id_cargo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
