effd4fe7869e1579588268bcebd004db182eeb02
ZenKySSJ <78515792+zenkyssj@users.noreply.github.com>
2025-09-08 15:10:28 -0300

    JPA implementado en los Repositorys

--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/admin/repository/SystemAdminRepository.java
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/admin/repository/SystemAdminRepository.java
@@ -1,4 +1,10 @@
 package com.backend.portalroshkabackend.admin.repository;
 
-public class SystemAdminRepository {
+
+import com.backend.portalroshkabackend.common.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SystemAdminRepository extends JpaRepository<Usuario, Integer> {
 }
