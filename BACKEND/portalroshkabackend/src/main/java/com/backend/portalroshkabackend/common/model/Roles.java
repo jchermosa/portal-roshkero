b3adbfebda798210468212190695252813f390ff
ZenKySSJ <78515792+zenkyssj@users.noreply.github.com>
2025-09-08 14:36:28 -0300

    Creando Modelo Roles en common/model

---
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/common/model/Roles.java
@@ -0,0 +1,24 @@
+package com.backend.portalroshkabackend.common.model;
+
+public class Roles {
+
+    private int id_rol;
+
+    private String nombre;
+
+    public int getId_rol() {
+        return id_rol;
+    }
+
+    public void setId_rol(int id_rol) {
+        this.id_rol = id_rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
