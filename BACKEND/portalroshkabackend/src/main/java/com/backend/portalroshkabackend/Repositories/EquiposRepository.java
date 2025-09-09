1cc370e33f44fa0901ba83c3bd3830da18261263
xdev <xdevofc@gmail.com>
2025-09-09 13:51:43 -0300

    cambiando de object a equipos

--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Repositories/EquiposRepository.java
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Repositories/EquiposRepository.java
@@ -3,8 +3,10 @@ package com.backend.portalroshkabackend.Repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface EquiposRepository extends CrudRepository<Object, Integer> {
+import com.backend.portalroshkabackend.Models.Equipos;
 
-    
+@Repository
+public interface EquiposRepository extends CrudRepository<Equipos, Integer> {
+
+
 }
