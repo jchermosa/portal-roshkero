17a35dc77327448067c4caca4e9930e471023659
Vladimir Gromov <vovamakukha@gmail.com>
2025-09-10 09:58:47 -0300

    try merge

--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/admin/repository/PositionsRepository.java
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/admin/repository/PositionsRepository.java
@@ -2,6 +2,7 @@ package com.backend.portalroshkabackend.admin.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.backend.portalroshkabackend.common.model.Cargos;
 
 @Repository
 public interface PositionsRepository extends JpaRepository<Cargos, Integer> {
