2b4340296b4ecdaeccdf5e15e3e7a7550b14c889
ZenKySSJ <78515792+zenkyssj@users.noreply.github.com>
2025-09-10 10:36:32 -0300

    Fix en HumanResourceController

--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/common/model/Cargos.java
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/common/model/Cargos.java
@@ -13,13 +13,14 @@ public class Cargos {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int idСargo;
+    @Column(name = "id_cargo")
+    private int idCargo;
 
-    @Column(nullable = false)
+    @Column(name = "nombre", nullable = false)
     private String nombre;
 
-    public int getIdCargo() { return idСargo; }
-    public void setId_cargo(int idСargo) { this.idСargo = idСargo; }
+    public int getIdCargo() { return idCargo; }
+    public void setId_cargo(int idCargo) { this.idCargo = idCargo; }
 
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
