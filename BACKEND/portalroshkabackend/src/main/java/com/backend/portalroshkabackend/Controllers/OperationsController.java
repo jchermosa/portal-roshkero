9270a31792925327384e5f2af5b4f980e663507d
xdev <xdevofc@gmail.com>
2025-09-10 16:42:43 -0300

    eliminando un import

--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Controllers/OperationsController.java
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Controllers/OperationsController.java
@@ -12,6 +12,7 @@ import com.backend.portalroshkabackend.Services.IOperationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.data.domain.Page;
