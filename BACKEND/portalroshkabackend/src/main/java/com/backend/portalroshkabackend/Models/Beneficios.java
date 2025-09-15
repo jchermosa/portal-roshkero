a6a17c0a559ceb8f024012116aaa72c5c8500adc
xdev <xdevofc@gmail.com>
2025-09-14 23:30:40 -0300

    creand el modelo beneficios

---
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Models/Beneficios.java
@@ -0,0 +1,38 @@
+package com.backend.portalroshkabackend.Models;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name = "beneficio")
+public class Beneficios {
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_beneficio")
+    private Integer idBeneficio;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @Column(name = "inicio_vigencia")
+    private Date inicioVigencia;
+
+    @Column(name = "fin_vigencia")
+    private Date finVigencia;
+
+}
