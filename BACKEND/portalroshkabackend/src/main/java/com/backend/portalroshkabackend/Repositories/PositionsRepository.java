32744635117d2b8ee3e36c9637e2854cffea6534
xdev <xdevofc@gmail.com>
2025-09-15 08:37:26 -0300

    eliminando duplicados

--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Repositories/PositionsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.portalroshkabackend.Repositories;
-
-import com.backend.portalroshkabackend.Models.Cargos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PositionsRepository extends JpaRepository<Cargos, Integer> {
-}
--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Services/CargosService.java
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Services/CargosService.java
@@ -2,12 +2,12 @@ package com.backend.portalroshkabackend.Services;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.backend.portalroshkabackend.Models.Cargos;
-import com.backend.portalroshkabackend.Repositories.CargoRepository;
+import com.backend.portalroshkabackend.Repositories.CargosRepository;
 
 public class CargosService {
 
     @Autowired
-    private CargoRepository cargoRepository;
+    private CargosRepository cargoRepository;
 
     public Iterable<Cargos> getAllCargos() {
         return cargoRepository.findAll();
