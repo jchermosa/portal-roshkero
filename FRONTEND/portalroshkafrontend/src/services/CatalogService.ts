439143888ef5c19e4a6cbfb6757c93a3febce8b7
noe955747-stack <noe955747@gmail.com>
2025-09-18 10:38:52 -0300

    Catalogo

--- FRONTEND/portalroshkafrontend/src/services/CatalogService.ts
+++ FRONTEND/portalroshkafrontend/src/services/CatalogService.ts
@@ -31,30 +31,7 @@ async function getCargosApi(token: string): Promise<CargoItem[]> {
   if (!res.ok) throw new Error(await res.text());
   return res.json();
 }
-//Catalgos nuevos para solicitud
-export async function getPermisos(
-  token: string,
-  _params: Record<string, string | number | undefined> = {}
-): Promise<CatalogItem[]> {
-  const res = await fetch(`/api/catalogos/tipos-permiso`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
 
-  if (!res.ok) throw new Error(await res.text());
-  return res.json();
-}
-
-export async function getBeneficio(
-  token: string,
-  _params: Record<string, string | number | undefined> = {}
-): Promise<CatalogItem[]> {
-  const res = await fetch(`/api/catalogos/tipos-beneficio`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-
-  if (!res.ok) throw new Error(await res.text());
-  return res.json();
-}
 // ================================
 // Mocks
 // ================================
