1d8ef90e8015b4cf9637323b6582390b8cb1e3c0
ZenKySSJ <78515792+zenkyssj@users.noreply.github.com>
2025-09-27 12:28:25 -0300

    Cambios en la logica de aceptar vacaciones y permisos

--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Repositories/TH/VacacionesAsignadasRepository.java
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Repositories/TH/VacacionesAsignadasRepository.java
@@ -3,5 +3,9 @@ package com.backend.portalroshkabackend.Repositories.TH;
 import com.backend.portalroshkabackend.Models.VacacionesAsignadas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface VacacionesAsignadasRepository extends JpaRepository<VacacionesAsignadas, Integer> {
+
+    Optional<VacacionesAsignadas> findBySolicitud_idSolicitud(Integer idSolicitud);
 }
