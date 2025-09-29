44ae41732d42f8723b885c2f228452b02f31bf1a
Vladimir Gromov <vovamakukha@gmail.com>
2025-09-28 21:28:37 -0300

    refacor metadatas

--- BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Repositories/TH/CargosRepository.java
+++ BACKEND/portalroshkabackend/src/main/java/com/backend/portalroshkabackend/Repositories/TH/CargosRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CargosRepository extends JpaRepository<Cargos, Integer> {
-
+    Optional<Cargos> findByNombre(String nombre);// for Vladimir
 
     Boolean existsByNombre(String nombre);
+
     Boolean existsByNombreAndIdCargoNot(String nombre, Integer idCargo);
 }
